97214e31a8d351a507c6df6a0e2fca8d5d9910a3
Yury Delendik <ydelendik@mozilla.com>
2018-10-30 17:35:56 -0500

    Fix sigmoid.js

--- thunder-demos1/sigmoid.js
+++ thunder-demos1/sigmoid.js
@@ -5,24 +5,30 @@ function sigmoid_js(x) {
 
 let sigmoid_wasm;
 
+function print(type, msg) {
+  document.getElementById("term").textContent += `[${type}]\t${msg}\n`;
+}
+
 function test_wasm() {
   const start = performance.now();
   let sum = 0;
-  for (let i = -1000000; i <= 1000000; i++)
+  for (let i = -16; i <= 16; i += 1e-5)
     sum += sigmoid_wasm(i) - 0.5;
-  console.log("wasm", sum, performance.now() - start);
+  print("wasm", performance.now() - start);
+  console.log("wasm", sum);
 }
 
 function test_js() {
   const start = performance.now();
   let sum = 0; 
-  for (let i = -1000000; i <= 1000000; i++)
+  for (let i = -16; i <= 16; i += 1e-5)
     sum += sigmoid_js(i) - 0.5;
-  console.log("js", sum, performance.now() - start);
+  print("js", performance.now() - start);
+  console.log("wasm", sum);
 }
 
 function run() {
-  test_js(); test_js(); test_js();test_js(); test_js(); test_js(); test_js();
+  test_js();   test_js();   test_js();   test_js();   test_js();   test_js();   test_js();
   test_wasm(); test_wasm(); test_wasm(); test_wasm(); test_wasm(); test_wasm(); test_wasm();
 }
 
@@ -34,6 +40,6 @@ async function start() {
   run();
   setTimeout(run, 1000);
   setTimeout(run, 2000);
-  setTimeout(run, 10000);
+  setTimeout(run, 5000);
 }
 start();
